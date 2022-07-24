400b8cb6ddae6f230445307c937d12595d1356e6
gaoey <gaoey@hotmail.com>
2022-07-24 12:08:19 +0700

    feat: add list

---
+++ src/concepts/xbox/list_test.rs
@@ -0,0 +1,30 @@
+#[cfg(test)]
+mod test {
+    use crate::concepts::xbox::list::List::{Cons, Nil};
+
+    #[test]
+    fn test_list() {
+        /*
+        box is pointer
+        see more: The Stack and the Heap
+        https://web.mit.edu/rust-lang_v1.25/arch/amd64_ubuntu1404/share/doc/rust/html/book/first-edition/the-stack-and-the-heap.html
+        */
+        let correct_list = Cons(2, Box::new(Cons(3, Box::new(Nil))));
+        println!("{:?}", correct_list);
+    }
+
+    #[ignore]
+    #[test]
+    fn test_failed_list() {
+        /*
+        error shown: "recursive without indirection"
+        indirection mean rust compiler doesn't know exactly memory size of FailedList
+        direction mean rust compiler know exactly which memory size of particular parameter
+        */
+        // #[derive(Debug)]
+        // pub enum FailedList<T> {
+        //     FCons(T, FailedList<T>),
+        //     FNil,
+        // }
+    }
+}
---
+++ src/concepts/xbox/mod.rs
@@ -0,0 +1,2 @@
+mod list;
+mod list_test;
